4e4e8b65c5eebcf43f5d396f29edc97ad3e3f669
Ivan Tishchenko <tivan@student.ethz.ch>
2018-06-12 11:07:04 +0200

    Added the plots for the best run

--- plots/plot.py
+++ plots/plot.py
@@ -25,62 +25,83 @@ def smooth(y, box_pts):
 '''
 Plot different models loss on the same graph
 '''
-def plot_loss(in_files, out_file, names, colors):
+def plot_loss(in_files, out_file, names, colors, smooth_val):
     num_models = len(in_files)
     limits_x = []
+    # limits_y = []
     for i in range(num_models):
         steps, values = extract_data(in_files[i])
         limits_x.append(max(steps))
-        plt.plot(steps, smooth(values, 5),  color=colors[i], alpha=0.8, label=names[i])
+        # limits_y.append(max(values))
+        if smooth_val[i] != -1:
+            smooth_val = smooth(values, smooth_val[i])
+        else:
+            smooth_val = values
+        plt.plot(steps, smooth_val, color=colors[i], alpha=0.8, label=names[i])
 
     plt.ylim(ymin=0)
-    plt.ylim(ymax=1000)
+    # plt.ylim(ymax=max(limits_y))
+    plt.ylim(ymax=500)
     plt.xlim(xmin=0)
     plt.xlim(xmax=max(limits_x))
     plt.grid(linestyle='dashed')
     plt.xlabel('Batch')
     plt.ylabel('Loss')
-    plt.legend()
+    # plt.legend()
     plt.savefig(out_file)
 
 '''
 Plot different models acc on the same graph
 '''
-def plot_acc(in_files, out_file, names, colors):
+def plot_acc(in_files, out_file, names, colors, smooth_val):
     num_models = len(in_files)
     limits_x = []
     limits_y = []
     for i in range(num_models):
         steps, values = extract_data(in_files[i])
         limits_x.append(max(steps))
-        limits_y.append(max(values))
-        plt.plot(steps, smooth(values, 5),  color=colors[i], alpha=0.8)
+        # limits_y.append(max(values))
+
+        if smooth_val[i] != -1:
+            smooth_val = smooth(values, smooth_val[i])
+        else:
+            smooth_val = values
+        plt.plot(steps, smooth_val, color=colors[i], alpha=0.8)
 
     plt.ylim(ymin=0)
-    plt.ylim(ymax=max(limits_y))
+    # plt.ylim(ymax=max(limits_y))
+    plt.ylim(ymax=0.31)
     plt.xlim(xmin=0)
     plt.xlim(xmax=max(limits_x))
     plt.grid(linestyle='dashed')
     plt.xlabel('Batch')
     plt.ylabel('Accuracy')
-    plt.legend(names)
+    # plt.legend(names)
     plt.savefig(out_file)
     plt.gcf().clear()
 
 
 # Model Comparison plots
-plot_loss([DATA_DIR + 'mvit_loss.csv', DATA_DIR + "incept_loss.csv"],
-          OUT_DIR + 'loss.png',
-          ["MvitNet", "InceptionNet"],
-          ['b', 'r'])
-
-plot_acc([DATA_DIR + 'mvit_acc.csv', DATA_DIR + "incept_acc.csv"],
-         OUT_DIR + 'acc.png',
-         ["MvitNet", "InceptionNet"],
-         ['b', 'r'])
-
-# single
-plot_loss([DATA_DIR + 'mvit_loss.csv'],
-          OUT_DIR + 'test.png',
+plot_loss([DATA_DIR + "train_loss.csv"],
+          OUT_DIR + 'train_loss.png',
           ["MvitNet"],
-          ['b'])
+          ['b'],
+          [9])
+
+plot_acc([DATA_DIR + "train_acc.csv"],
+         OUT_DIR + 'train_acc.png',
+         ["MvitNet"],
+         ['b'],
+         [9])
+
+plot_loss([DATA_DIR + "test_loss.csv"],
+          OUT_DIR + 'test_loss.png',
+          ["MvitNet"],
+          ['b'],
+          [-1])
+
+plot_acc([DATA_DIR + "test_acc.csv"],
+         OUT_DIR + 'test_acc.png',
+         ["MvitNet"],
+         ['b'],
+         [-1])
