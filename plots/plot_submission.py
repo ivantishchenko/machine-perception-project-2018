c4d7cb6813ca7ef4bf4e2d7797efc19197ed8d68
Ivan Tishchenko <tivan@student.ethz.ch>
2018-06-13 12:56:00 +0200

    Added the script for plotting submissions

---
+++ plots/plot_submission.py
@@ -0,0 +1,52 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+colours = {0: 'black', 1: 'blue', 2: 'orange', 3: 'green', 4: 'red', 5: 'yellow'}
+
+DATASET_PATH = '../datasets/testing.h5'
+SUBMISSION_PATH = 'sub/'
+OUT_PATH = 'img/'
+N = 10
+
+
+def plot_submission(dataset, submission, out_pattern, select_idx, normalized=True):
+    with open(submission, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)  # skip header
+
+        buf = []
+        for row in reader:
+            buf.append(row)
+
+        for count in range(N):
+            kp_raw = buf[select_idx[count]][1:]
+            i = int(buf[select_idx[count]][0])
+            kp_np = np.asarray(kp_raw, dtype=np.float32)
+            kp_np = np.reshape(kp_np, (21, 2))
+            img = dataset['img'][i]
+            img = img.transpose(1, 2, 0)
+            kpx = kp_np[:, 0]
+            kpy = kp_np[:, 1]
+            if normalized:
+                img = img / 255
+            plt.imshow(img)
+            # Root of hand drawn
+            plt.scatter(kpx[0], kpy[0], color=colours[0])
+            # Attach all knuckles to root of hand, then draw then fingers
+            for i in range(1, 6):
+                plt.plot([kpx[0], kpx[i * 4]], [kpy[0], kpy[i * 4]], color=colours[0])
+                plt.plot(kpx[i * 4 - 3:(i + 1) * 4 - 3], kpy[i * 4 - 3:(i + 1) * 4 - 3], marker='o', color=colours[i])
+            plt.savefig(out_pattern + str(count) + '.png', bbox_inches='tight')
+            # plt.savefig(OUT_PATTERN + str(count) + '.png')
+            plt.gcf().clear()
+
+
+g = h5py.File(DATASET_PATH, 'r')
+testset = g['test']
+
+select_idx = np.random.random_integers(0, len(testset['img']) - 1, 10)
+
+# plot submissions
+plot_submission(testset, SUBMISSION_PATH + 'submission.csv', OUT_PATH + 'res', select_idx)
